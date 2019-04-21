dc1eece3a5685ca822ae296712be7f32e1a366cb
Kyle Sutton <kylesutton@dhcp-10-250-30-115.harvard.edu>
2019-04-21 16:17:29 -0400

    maybe this works??

--- src/Template1-canonical/index.js
+++ src/Template1-canonical/index.js
@@ -113,9 +113,7 @@ class BlogPostTemplate extends Component {
         {coverPhoto && isPreview && <img src={coverPhoto} alt="Cover Photo" />}
         {/* Post Content Section */}
         <PostContent
-          className={`${styles.BlogPost__content} ${
-            styles.bodytext
-          } ql-editor `}
+          className={`${styles.BlogPost__content} ${styles.bodytext}`}
           content={htmlBody}
         />
         {/* Tags Section */}
